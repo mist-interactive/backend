1f9c16e63bc201d3deed67d693281534092402a7
nraatika <nraatika@student.hive.fi>
2026-07-03 16:28:50 +0300

    refactor(db): moved migration code to be separate from DB setup
    Signed-off-by: nraatika <nraatika@student.hive.fi>

--- go-server/db/initdb.go
+++ go-server/db/initdb.go
@@ -3,9 +3,7 @@ package db
 import (
 	"context"
 	"database/sql"
-	"embed"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
 	"strings"
@@ -15,14 +13,7 @@ import (
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
-	"github.com/uptrace/bun/migrate"
 )
-
-// set up migrations
-//
-//go:embed migrations/*.sql
-var migrationFS embed.FS
-var Migrations = migrate.NewMigrations()
 
 func InitDB() (*bun.DB, error) {
 	_ = godotenv.Load("../.env")
@@ -68,42 +59,4 @@ func getDBPassword() string {
 		return strings.TrimSpace(string(content))
 	}
 	return ""
-}
-
-func RunMigrations(ctx context.Context, db *bun.DB) error {
-	fsys, err := fs.Sub(migrationFS, "migrations")
-	if err != nil {
-		return err
-	}
-	err = Migrations.Discover(fsys)
-	if err != nil {
-		return err
-	}
-
-	migrator := migrate.NewMigrator(db, Migrations)
-	migrator.Lock(ctx)
-	defer func() { //need a detached context for unlock, to ensure it can run if the passed-in context gets cancelled
-		unlockCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		_ = migrator.Unlock(unlockCtx)
-	}()
-
-	if err = migrator.Init(ctx); err != nil {
-		return err
-	}
-
-	var group *migrate.MigrationGroup
-	group, err = migrator.Migrate(ctx)
-	if err != nil {
-		return err
-	}
-	if len(group.Migrations) == 0 {
-		log.Println("No migrations applied.")
-		return nil
-	}
-	log.Printf("Applied migration set #%d:\n", group.ID)
-	for _, m := range group.Migrations {
-		log.Printf("  └─ ✓ Applied: % s\n", m.Name)
-	}
-	return nil
 }
