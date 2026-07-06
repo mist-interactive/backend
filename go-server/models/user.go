eb3cfb08e9a8976142a9f52ddeaa83baeb4c70d6
nraatika <nraatika@student.hive.fi>
2026-07-06 15:58:17 +0300

    refactor(models): updated User struct to refelct the added columns
    Signed-off-by: nraatika <nraatika@student.hive.fi>

--- go-server/models/user.go
+++ go-server/models/user.go
@@ -9,9 +9,12 @@ import (
 type User struct {
 	bun.BaseModel `bun:"table:users"`
 
-	ID        int64     `bun:"id,pk,autoincrement" json:"id"`
-	Username  string    `bun:"username,type:varchar(50),unique,notnull" json:"username"`
-	PWHash    string    `bun:"password_hash,type:varchar(255),notnull" json:"-"`
-	Email     string    `bun:"email,type:varchar(255),unique,notnull" json:"email"`
-	CreatedAt time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp" json:"created_at"`
+	ID          int64     `bun:"id,pk,autoincrement" json:"id"`
+	Username    string    `bun:"username,type:varchar(50),unique,notnull" json:"username"`
+	PWHash      string    `bun:"password_hash,type:varchar(255),notnull" json:"-"`
+	Email       string    `bun:"email,type:varchar(255),unique,notnull" json:"email"`
+	CreatedAt   time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp" json:"created_at"`
+	TotalWins   int32     `bun:"total_wins" json:"total_wins"`
+	TotalLosses int32     `bun:"total_losses" json:"total_losses"`
+	AvatarURL   string    `bun:"avatar_url" json:"avatar_url"`
 }
