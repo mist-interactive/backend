70d05235886fc45806ab9eb6cd7e0eb9510f4495
nraatika <nraatika@student.hive.fi>
2026-07-29 19:42:18 +0300

    feat(database): migration file creating a  table in db, and accompanying struct definition

---
+++ go-server/models/match.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+type MatchStatus string
+type MatchResult string
+
+const (
+	StatusInProgress MatchStatus = "in_progress"
+	StatusFinished   MatchStatus = "finished"
+	StatusAbandoned  MatchStatus = "abandoned"
+
+	ResultPlayer1Win MatchResult = "player1_win"
+	ResultPlayer2Win MatchResult = "player2_win"
+	ResultDraw       MatchResult = "draw"
+	ResultAborted    MatchResult = "aborted"
+)
+
+type MatchRecord struct {
+	bun.BaseModel `bun:"table:matches"`
+
+	ID         int64       `json:"id" bun:"id,pk,autoincrement"`
+	Player1    int64       `json:"player_one" bun:"player_one,notnull"`
+	Player2    int64       `json:"player_two" bun:"player_two,notnull"`
+	Status     MatchStatus `json:"status" bun:"status,notnull"`
+	Result     MatchResult `json:"result" bun:"result"`
+	StartedAt  time.Time   `json:"started_at" bun:"started_at,default:current_timestamp"`
+	FinishedAt *time.Time  `json:"finished_at" bun:"finished_at"`
+}
