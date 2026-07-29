a256b3bc97e36d0e06bd9830820dd6d79f74cb54
nraatika <nraatika@student.hive.fi>
2026-07-29 19:44:41 +0300

    wip: matches endpoint

--- go-server/handlers/matches.go
+++ go-server/handlers/matches.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
+	"context"
+	"database/sql"
 	"dbBackend/models"
 	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/uptrace/bun"
 )
@@ -17,17 +21,23 @@ type MatchCreateInput struct {
 	Player2 int64 `json:"player_two" validate:"required"`
 }
 
+type MatchPatchInput struct {
+	Player1 int64  `json:"player_one" validate:"required"`
+	Player2 int64  `json:"player_two" validate:"required"`
+	Result  string `json:"result" validate:"required,oneof=player1_win player2_win draw aborted"`
+	Status  string `json:"status" validate:"required,oneof=finished abandoned"`
+}
+
 func (h *MatchHandler) MatchCreate(w http.ResponseWriter, r *http.Request) {
 	input, err := DecodeAndValidate[MatchCreateInput](r)
-	if err != nil {
-		http.Error(w, "Problem validating request", http.StatusBadRequest)
-		return
-	}
 	if input.Player1 == input.Player2 {
 		http.Error(w, "Players cannot play themselves", http.StatusConflict)
 		return
 	}
-
+	if err != nil {
+		http.Error(w, "Problem validating request", http.StatusBadRequest)
+		return
+	}
 	match := &models.MatchRecord{
 		Player1: input.Player1,
 		Player2: input.Player2,
@@ -37,7 +47,7 @@ func (h *MatchHandler) MatchCreate(w http.ResponseWriter, r *http.Request) {
 		Model(match).
 		Scan(r.Context())
 	if err != nil {
-		http.Error(w, "Failed to create match entry in database"+err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Failed to create match entry in database: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -46,4 +56,93 @@ func (h *MatchHandler) MatchCreate(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]any{
 		"id": match.ID,
 	})
+}
+
+func (h *MatchHandler) MatchPatch(w http.ResponseWriter, r *http.Request) {
+	input, err := DecodeAndValidate[MatchPatchInput](r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	match := &models.MatchRecord{
+		Player1:   input.Player1,
+		Player2:   input.Player2,
+		Result:    input.Result,
+		StartedAt: input.StartedAt,
+	}
+	if match.Result != "aborted" {
+		now := time.Now()
+		match.FinishedAt = &now
+	}
+
+	tx, err := h.DB.BeginTx(r.Context(), &sql.TxOptions{})
+	if err != nil {
+		http.Error(w, "Failed to start stats transaction in database", http.StatusInternalServerError)
+	}
+	defer tx.Rollback()
+	_, err = tx.NewInsert().Model(match).Exec(r.Context())
+	if err != nil {
+		if HandleDBConflict(w, err) {
+			return
+		}
+		http.Error(w, "Database error: Failed to record match history", http.StatusInternalServerError)
+		return
+	}
+	err = updatePlayerProfiles(r.Context(), tx, match)
+	if err != nil {
+		log.Printf("Error updating player stats for match %d: %v", match.ID, err)
+		http.Error(w, "Database error: Failed to record match history", http.StatusInternalServerError)
+		return
+	}
+	if err := tx.Commit(); err != nil {
+		http.Error(w, "Failed to save match transaction to database", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(match)
+}
+
+func updatePlayerProfiles(ctx context.Context, tx bun.Tx, mr *models.MatchRecord) error {
+	switch mr.Result {
+	case "player1_win":
+		_, err := tx.NewUpdate().
+			Model((*models.User)(nil)).
+			Set("total_wins = total_wins + 1").
+			Where("id = ?", mr.Player1).
+			Exec(ctx)
+		if err != nil {
+			return err
+		}
+		_, err = tx.NewUpdate().
+			Model((*models.User)(nil)).
+			Set("total_losses = total_losses + 1").
+			Where("id = ?", mr.Player2).
+			Exec(ctx)
+		if err != nil {
+			return err
+		}
+	case "player2_win":
+		_, err := tx.NewUpdate().
+			Model((*models.User)(nil)).
+			Set("total_wins = total_wins + 1").
+			Where("id = ?", mr.Player2).
+			Exec(ctx)
+		if err != nil {
+			return err
+		}
+		_, err = tx.NewUpdate().
+			Model((*models.User)(nil)).
+			Set("total_losses = total_losses + 1").
+			Where("id = ?", mr.Player1).
+			Exec(ctx)
+		if err != nil {
+			return err
+		}
+	case "draw", "aborted":
+		log.Printf("Warning: Draws and aborted matches cause no stat updates: %s", mr.Result)
+	default:
+		log.Printf("Warning: Unhandled match result condition encountered: %s", mr.Result)
+	}
+	return nil
 }
