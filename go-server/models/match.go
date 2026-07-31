b8cbc0984304a2142e6471a7c2f7d0ea825bf4af
nraatika <nraatika@student.hive.fi>
2026-07-31 17:21:07 +0300

    fix(API): changed MatchRecord struct field  to be a pointer to allow a NULL value fic(seeding): restored proper error handling to seeding the DB with users

--- go-server/models/match.go
+++ go-server/models/match.go
@@ -23,11 +23,11 @@ const (
 type MatchRecord struct {
 	bun.BaseModel `bun:"table:matches"`
 
-	ID         int64       `json:"id" bun:"id,pk,autoincrement"`
-	Player1    int64       `json:"player_one" bun:"player_one,notnull"`
-	Player2    int64       `json:"player_two" bun:"player_two,notnull"`
-	Status     MatchStatus `json:"status" bun:"status,notnull"`
-	Result     MatchResult `json:"result" bun:"result"`
-	StartedAt  time.Time   `json:"started_at" bun:"started_at,default:current_timestamp"`
-	FinishedAt *time.Time  `json:"finished_at" bun:"finished_at"`
+	ID         int64        `json:"id" bun:"id,pk,autoincrement"`
+	Player1    int64        `json:"player_one" bun:"player_one,notnull"`
+	Player2    int64        `json:"player_two" bun:"player_two,notnull"`
+	Status     MatchStatus  `json:"status" bun:"status,notnull"`
+	Result     *MatchResult `json:"result" bun:"result"`
+	StartedAt  time.Time    `json:"started_at" bun:"started_at,default:current_timestamp"`
+	FinishedAt *time.Time   `json:"finished_at" bun:"finished_at"`
 }
