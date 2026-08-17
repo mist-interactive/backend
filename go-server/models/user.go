b89ed221f71f6d4f148bda2259b36073f64617d0
nraatika <nraatika@student.hive.fi>
2026-08-17 14:37:12 +0300

    feat(backend) Updated the User model struct to reflect the changes in the DB

--- go-server/models/user.go
+++ go-server/models/user.go
@@ -13,5 +13,8 @@ type User struct {
 	Username  string    `bun:"username,type:varchar(50),unique,notnull" json:"username"`
 	PWHash    string    `bun:"password_hash,type:varchar(255),notnull" json:"-"`
 	Email     string    `bun:"email,type:varchar(255),unique,notnull" json:"email"`
+	Bio       string    `bun:"bio" json:"bio"`
+	AvatarURL *string   `bun:"avatar_url" json:"avatarUrl"` //pointer so it can be null
 	CreatedAt time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp" json:"created_at"`
+	UpdatedAt time.Time `bun:"updated_at,nullzero,default:current_timestamp" json:"updated_at"`
 }
