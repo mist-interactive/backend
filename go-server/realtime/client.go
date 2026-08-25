28b8048318399353263bfccb1b89af3c12c4af91
nraatika <nraatika@student.hive.fi>
2026-08-25 16:14:22 +0300

    feat(websockets): defined Client struct and function prototypes

---
+++ go-server/realtime/client.go
@@ -0,0 +1,31 @@
+package realtime
+
+import "github.com/gorilla/websocket"
+
+const sendBufferSize = 256
+
+type Client struct {
+	Hub      *Hub
+	Conn     *websocket.Conn //handles network traffic
+	UserID   int64
+	Username string
+	Send     chan []byte // Buffered channel of data waiting to be sent to client
+}
+
+func NewClient(hub *Hub, conn *websocket.Conn, userID int64, username string) *Client {
+	return &Client{
+		Hub:      hub,
+		Conn:     conn,
+		UserID:   userID,
+		Username: username,
+		Send:     make(chan []byte, sendBufferSize),
+	}
+}
+
+func (c *Client) writePump() {
+	//TODO: listens on Send channel, and sends it over Conn when there is a message
+}
+
+func (c *Client) readPump() {
+	//TODO: listens to the Conn, and passes any messages the client sends to the Hub
+}
