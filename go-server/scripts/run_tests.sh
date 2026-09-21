f8d44a579a5c9f63e6c46d64d521d5daab96fcd8
nraatika <nraatika@student.hive.fi>
2026-09-21 16:44:56 +0300

    test(scripts): enable race detector for realtime package tests

--- go-server/scripts/run_tests.sh
+++ go-server/scripts/run_tests.sh
@@ -3,4 +3,4 @@ set -e
 
 docker compose exec -T go-server go test -v -count=1 ./handlers
 docker compose exec -T go-server go test -v -count=1 ./models
-docker compose exec -T go-server go test -v -count=1 ./realtime
+docker compose exec -T go-server go test -race -v -count=1 ./realtime
