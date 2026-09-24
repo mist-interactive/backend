0c682464ce75ab86bf535f16b4116f15746cea7d
nraatika <nraatika@student.hive.fi>
2026-09-24 12:16:49 +0300

    feat(data models): define Comment model and DTOs

---
+++ go-server/models/comment.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+type Comment struct {
+	bun.BaseModel `bun:"table:comments"`
+
+	ID        int64     `json:"id" bun:"id,pk,autoincrement"`
+	OwnerID   int64     `json:"owner_id" bun:"owner_id,notnull"`
+	PosterID  int64     `json:"poster_id" bun:"poster_id,notnull"`
+	Content   string    `json:"content" bun:"content,notnull" validate:"required,min=1,max=1000"`
+	CreatedAt time.Time `json:"created_at" bun:"created_at,default:current_timestamp"`
+}
+
+type CommentCreateInput struct {
+	Content string `json:"content" validate:"required,min=1,max=1000"`
+}
+
+type CommentResponse struct {
+	ID              int64     `json:"id" bun:"id"`
+	OwnerID         int64     `json:"owner_id" bun:"owner_id"`
+	PosterID        int64     `json:"poster_id" bun:"poster_id"`
+	PosterUsername  string    `json:"poster_username" bun:"poster_username"`
+	PosterAvatarURL *string   `json:"poster_avatar_url" bun:"poster_avatar_url"`
+	Content         string    `json:"content" bun:"content"`
+	CreatedAt       time.Time `json:"created_at" bun:"created_at"`
+}
+
+type PaginatedCommentsResponse struct {
+	Comments []CommentResponse `json:"comments"`
+	HasMore  bool              `json:"has_more"`
+}
+
